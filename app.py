6b9fca8847563ebad494fe99c9ad8db80cb5a0ea
Abhayraj-pipesort <abhay@pipesort.com>
2023-10-17 14:26:18 +0530

    feat: added code for using palm api for text generation and embeddings with llama-index

---
+++ app.py
@@ -0,0 +1,26 @@
+import os
+from dotenv import load_dotenv
+from llama_index import (
+    VectorStoreIndex,
+    SimpleDirectoryReader,
+    LLMPredictor,
+    ServiceContext,
+)
+from llama_index.llms import PaLM
+from langchain.embeddings.google_palm import GooglePalmEmbeddings
+
+load_dotenv()
+
+model = "models/text-bison-001"
+embeddings = GooglePalmEmbeddings(google_api_key=os.getenv("PALM_API_KEY"))
+
+documents = SimpleDirectoryReader("data").load_data()
+
+llm = PaLM(api_key=os.getenv("PALM_API_KEY"), model_name=model)
+service_context = ServiceContext.from_defaults(llm=llm, embed_model=embeddings)
+
+index = VectorStoreIndex.from_documents(documents, service_context=service_context)
+
+query_engine = index.as_query_engine()
+response = query_engine.query("What is the moral of the story")
+print(response)
